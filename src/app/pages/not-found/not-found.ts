457139852411f70a07a48462c2f3532d19cf32b1
La svedese <5554930+sarasjodin@users.noreply.github.com>
2025-06-12 08:37:51 +0200

    Added not-found functionality

--- src/app/pages/not-found/not-found.ts
+++ src/app/pages/not-found/not-found.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './not-found.html',
-  styleUrl: './not-found.css'
+  styleUrl: './not-found.css',
+  standalone: true,
 })
-export class NotFound {
-
-}
+export class NotFound {}
